b2581c04b8ae1c14d0d0b7bdccbc62f22a866bf8
kamlesh04yadav15-ux <kamlesh04yadav15@gmail.com>
2026-05-23 12:08:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,78 @@
+// =========================
+// DUBAI AI TAX MAIN JS
+// =========================
 
+
+
+// Navbar Button Click
+
+const startBtn =
+    document.querySelector(".start-btn");
+
+if(startBtn){
+
+    startBtn.addEventListener("click", function(){
+
+        window.location.href =
+            "vat.html";
+    });
+}
+
+
+
+// Primary Button
+
+const primaryBtn =
+    document.querySelector(".primary-btn");
+
+if(primaryBtn){
+
+    primaryBtn.addEventListener("click", function(){
+
+        window.location.href =
+            "vat.html";
+    });
+}
+
+
+
+// Secondary Button
+
+const secondaryBtn =
+    document.querySelector(".secondary-btn");
+
+if(secondaryBtn){
+
+    secondaryBtn.addEventListener("click", function(){
+
+        window.location.href =
+            "ai-assistant.html";
+    });
+}
+
+
+
+// Service Cards Animation
+
+const cards =
+    document.querySelectorAll(".service-card");
+
+cards.forEach(function(card){
+
+    card.addEventListener("mouseenter", function(){
+
+        card.style.transform =
+            "translateY(-10px)";
+
+        card.style.transition =
+            "0.3s";
+    });
+
+
+
+    card.addEventListener("mouseleave", function(){
+
+        card.style.transform =
+            "translateY(0px)";
+    });
+});
